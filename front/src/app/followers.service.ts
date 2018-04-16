e18fa7eade55fc2f28c70c44c13d33cc28d59e22
SvS <s1099971@student.hsleiden.nl>
2018-04-16 12:35:13 +0200

    adding front end to github

---
+++ front/src/app/followers.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import {Follower_model} from './Follower_model';
+import { Observable } from 'rxjs/Observable';
+import { of } from 'rxjs/observable/of';
+import {ApiService} from "../shared/api.service";
+
+@Injectable()
+export class FollowersService {
+  followersDay1 : Follower_model = {
+    day: "Monday",
+    amount: 263
+  };
+
+  followersDay2 : Follower_model;
+
+  constructor() { //Pass API Service here
+    console.log(this.followersDay1)
+    // this.apiService.get("localhost:8080/api/")
+  }
+
+  getFollowersDay1(): Observable<Follower_model> {
+    return of(this.followersDay1)
+  }
+
+  getFollowersDay2(): Observable<Follower_model> {
+    return of(this.followersDay2)
+  }
+
+
+
+}
+
